0bc26ecbc47b37d3004c45bc9d828974597d46f7
kch8293 <casey.hoh.kc@gmail.com>
2019-11-05 17:24:39 +0800

    add templates

--- server.py
+++ server.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return '<h1>Why so easy</h1>'
-
+   return render_template('index.html')
+   # by default looks for index.html inside a templates folder in the same directory as this script.
 
 @app.route("/another")
 def show():
