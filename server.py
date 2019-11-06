47ccc967ec47a87d875f02b50a725740576a48ec
kch8293 <casey.hoh.kc@gmail.com>
2019-11-06 13:51:54 +0800

    added non-functioning contact us form

--- server.py
+++ server.py
@@ -1,28 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    first_name = request.args.get('first_name')
     signed_in = True # we are hardcoding this just to demonstrate how we can do conditionals in our template files, in future we won't be hardcoding this.
     return render_template('index.html', signed_in=signed_in)
 
 @app.route("/<name>")
 def user_page(name):
     name = name.capitalize()
-    return render_template('hello.html', name = name)
+    signed_in = True
+    return render_template('hello.html', name = name, signed_in=signed_in)
     # by default looks for index.html inside a templates folder in the same directory as this script.
-
-@app.route("/another")
-def show():
-    return '<h1>Yo</h1>'
-
-@app.route('/user/<username>')
-def show_name(username):
-    return f'<h1>Hi {username}</h1>'
 
 @app.route("/contact")
 def contact():
-    return render_template('contact.html')
+    signed_in = True
+    return render_template('contact.html', signed_in=signed_in)
 
 if __name__ == '__main__':
     app.run(debug = True)
